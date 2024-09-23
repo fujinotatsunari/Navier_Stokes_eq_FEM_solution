15deb5583d33d3856f6e2ce5fbab953f4398cf9b
fujino <thickpaperwhite3@gmail.com>
2024-09-23 21:58:33 +0900

    ファイルの用意

--- 2d_flow_viewer/_2d_flow_viewer.py
+++ 2d_flow_viewer/_2d_flow_viewer.py
@@ -1,1 +1,14 @@
 
+#やること
+#1:スカラー場(圧力場と流れの強さ)の可視化(ひとまずこれ)
+#2:ベクトル場(流れ場)の可視化
+
+#必要なもの
+#1:ファイル入力(C:/Result/flow_view/input)に手動で(U,V,P,magnitude).csvを入れてもらう
+#2:ファイル出力(C:/Result/flow_view/output)に出す
+
+import numpy as np
+import matplotlib.pyplot as plt
+import csv
+import pprint
+
